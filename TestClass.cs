1c87a8584e691e80ab0b92af97150e48685ed905
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-05 13:53:04 +0200

    Create test case for numbers from 100 to 999

--- TestClass.cs
+++ TestClass.cs
@@ -62,13 +62,35 @@ namespace NumbersToWordsTDDKata
             Assert.AreEqual(expected, words);
         }
 
+        [TestCase("one hundred", 100)]
+        [TestCase("two hundred", 200)]
+        [TestCase("three hundred", 300)]
+        [TestCase("four hundred", 400)]
+        [TestCase("five hundred", 500)]
+        [TestCase("six hundred", 600)]
+        [TestCase("seven hundred", 700)]
+        [TestCase("eight hundred", 800)]
+        [TestCase("nine hundred", 900)]
+
+        [TestCase("one hundred twenty-one", 121)]
+        [TestCase("one hundred eleven", 111)]
+        [TestCase("nine hundred ninety-nine", 999)]
+
+        public void FromHundredToNineHundredNinetyNine_Test(string expected, int number)
+        {
+            string words = new NumbersToWords().FromHundredToNineHundredNinetyNine(number);
+            Assert.AreEqual(expected, words);
+        }
+
         public class NumbersToWords
         {
             private readonly Dictionary<int, string> words = new Dictionary<int, string>(){
                 { 0, "zero"}, {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight" },
                 { 9, "nine"}, {10, "ten"}, { 11, "eleven"}, {12, "twelve"}, {13, "thirteen"}, {14, "fourteen"}, {15, "fifteen"},
                 { 16, "sixteen"}, {17, "seventeen"}, {18, "eighteen"}, {19, "ninteen" }, {20, "twenty"}, {30, "thirty"},
-                { 40, "fourty"}, {50, "fifty"}, {60, "sixty"}, {70, "seventy"}, {80, "eighty"}, {90, "ninety" }};
+                { 40, "fourty"}, {50, "fifty"}, {60, "sixty"}, {70, "seventy"}, {80, "eighty"}, {90, "ninety" },
+                
+            };
             
             public NumbersToWords() { }
 
@@ -88,6 +110,27 @@ namespace NumbersToWordsTDDKata
 
                 return result;
             }
+
+            public string FromHundredToNineHundredNinetyNine(int number)
+            {
+                int r = number % 100;
+                string result =string.Empty;
+
+                if (number % 100 != 0)
+                {
+                    if (r <=19)
+                        result += words[number / 100] + " hundred " + FromZeroToNineteen(r);
+                    else
+                        result += words[number / 100] + " hundred " + FromTwentyToNinetyNine(r);
+                }
+                else
+                {
+                    result += words[number / 100] + " hundred";
+                }
+                
+                return result;
+            }
+
         }
     }
 }
